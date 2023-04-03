91c6be0e47a67b5e94b66eaf05c3ee0b675def8f
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-04-03 18:53:52 -0400

    reverseWordsInString #2

--- TwoPointers/reverseWordsInString.java
+++ TwoPointers/reverseWordsInString.java
@@ -1,5 +1,13 @@
 package TwoPointers;
 
 public class reverseWordsInString{
-
+    public String rev(String str){
+        int start = 0;
+        int end = str.length() - 1;
+        char[] ch = new char[str.length()];
+        while(start < str.length()){
+            ch[start++] = str.charAt(end--);
+        }
+        return String.valueOf(ch);
+    }
 }
--- main.java
+++ main.java
@@ -1,22 +1,16 @@
 import java.util.Scanner;
 
+import TwoPointers.reverseWordsInString;
 import TwoPointers.sumOfThreeValues;
 
 public class main {
     public static void main(String[] args) {
-        sumOfThreeValues sum = new sumOfThreeValues();
+        reverseWordsInString reverse = new reverseWordsInString();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter size of the array:");
-        int size = scanner.nextInt();
-        int[] nums = new int[size];
-        for(int i = 0; i < size; i++){
-            System.out.println("Enter element of the array");
-            nums[i] = scanner.nextInt();
-        }
-        System.out.println("Enter the target to be searched");
-        int target = scanner.nextInt();
+        System.out.println("Enter string to be reversed");
+        String str = scanner.nextLine();
+        String rev = reverse.rev(str);
         scanner.close();
-        boolean result = sum.findSumOfThree(nums, target);
-        System.out.println(result);
+        System.out.println(rev);
     }
 }
