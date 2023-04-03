c334dd829b133e6faf96811f2443fdf44e9d90e5
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-04-03 18:17:04 -0400

    sumOfThreeValues #1

--- TwoPointers/sumOfThreeValues.java
+++ TwoPointers/sumOfThreeValues.java
@@ -1,5 +1,24 @@
 package TwoPointers;
 
+import java.util.Arrays;
+
 public class sumOfThreeValues {
-    
+    public boolean findSumOfThree(int[] nums, int target){
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            int sum = 0;
+            while(left < right){
+                sum = nums[i] + nums[left] + nums[right];
+                if(sum < target)
+                    left++;
+                else if(sum > target)
+                    right--;
+                else if(sum == target)
+                    return true;
+            }
+        }
+        return false;
+    }
 }
--- main.java
+++ main.java
@@ -1,16 +1,22 @@
-
-package TwoPointers;
 import java.util.Scanner;
+
+import TwoPointers.sumOfThreeValues;
 
 public class main {
     public static void main(String[] args) {
-        validPalindrome vp = new validPalindrome();
+        sumOfThreeValues sum = new sumOfThreeValues();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string to check if palindrome:");
-        String str = scanner.nextLine();
+        System.out.println("Enter size of the array:");
+        int size = scanner.nextInt();
+        int[] nums = new int[size];
+        for(int i = 0; i < size; i++){
+            System.out.println("Enter element of the array");
+            nums[i] = scanner.nextInt();
+        }
+        System.out.println("Enter the target to be searched");
+        int target = scanner.nextInt();
         scanner.close();
-        boolean result;
-        result = vp.isPalindrome(str);
+        boolean result = sum.findSumOfThree(nums, target);
         System.out.println(result);
     }
 }
