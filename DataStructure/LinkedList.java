e7eaf6643f3d63433215d3d84eb157f93e177b4f
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-04-04 01:04:21 -0400

    linkedListCycle #2

--- DataStructure/LinkedList.java
+++ DataStructure/LinkedList.java
@@ -1,5 +1,17 @@
 package DataStructure;
 
 public class LinkedList {
+    public ListNode head;
     
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public ListNode insertOnTail(ListNode node, ListNode head){
+        ListNode copyOfHead = head;
+        while(copyOfHead.next != null)
+            copyOfHead = copyOfHead.next;
+        copyOfHead.next = node;
+        return head;
+    }
 }
