587a053346265f14d03f7e34de707604d41beccd
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-04-03 22:30:08 -0400

    validPalindromeTwo #1

--- TwoPointers/reverseWordsInStringV2.java
+++ TwoPointers/reverseWordsInStringV2.java
@@ -16,8 +16,11 @@ public class reverseWordsInStringV2{
 
             strRev(sb, start, end - 1);
 
-            start = end + 1;
-            ++end;
+            start = ++end;
+            
+            /*
+             * To move forward with the space.
+             */
         }
         return sb.toString();
     }
@@ -28,10 +31,18 @@ public class reverseWordsInStringV2{
             sb.setCharAt(startRev++, sb.charAt(endRev));
             sb.setCharAt(endRev--, temp);
         }
+        /*
+         * This is performing swap but on the whole string(builder)
+         */
     }
 
     public String cleanSpace(char[] a, int n){
-        String str = new String(a, 0, n);
+        //String str = new String(a, 0, n);
+        String str = String.valueOf(a);
+        /*
+         * Both of these methods convert a character array to a string
+         */
+        
 
         str = str.replaceAll("\\s+"," ").trim();
         /*
@@ -40,8 +51,22 @@ public class reverseWordsInStringV2{
          * 
          * The original regex expression is \s, but \ being a special
          * character we need to use \\
+         * 
+         * We have chained the trim method which removes leading and 
+         * trailing whitespaces
          */
 
         return str;
     }
 }
+
+/*
+ * Time Complexity:
+ * Traversing the array initially to reverse it - O(n)
+ * Traversing the array again to reverse each word - O(n)
+ * 
+ * Space Complexity:
+ * Creating stringbuilder - O(n)
+ * If we create another string becasue of cleanSpace - O(n)
+ * While returning the newly created string - O(n)
+ */
