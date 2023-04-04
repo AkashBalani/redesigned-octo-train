ef79ac2fd5fcfce20ffc9a71370d9ed2c686e109
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-04-04 10:30:30 -0400

    middleOfLinkedList #2

--- FastAndSlow/middleOfLinkedList.java
+++ FastAndSlow/middleOfLinkedList.java
@@ -1,5 +1,20 @@
 package FastAndSlow;
 
+import DataStructure.ListNode;
+
 public class middleOfLinkedList {
-    
+    public ListNode middle(ListNode head){
+        int len = 0;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(head != null){
+            len++;
+            head = head.next;
+        }
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return (len % 2 == 1) ? slow : slow.next;
+    }
 }
--- main.java
+++ main.java
@@ -3,19 +3,15 @@ import java.util.Scanner;
 import DataStructure.LinkedList;
 import DataStructure.ListNode;
 import FastAndSlow.linkedListCycle;
+import FastAndSlow.middleOfLinkedList;
 
 public class main {
     public static void main(String[] args) {
-        ListNode node = new ListNode(3);
+        ListNode node = new ListNode(1);
         LinkedList list = new LinkedList();
         ListNode head = node;
-        list.insertOnTail(new ListNode(3), head);
-        list.insertOnTail(new ListNode(5), head);
-        list.insertOnTail(new ListNode(9), head);
-        list.insertOnTail(new ListNode(11), head);
-        list.insertOnTail(new ListNode(17), head);
-        linkedListCycle cycle = new linkedListCycle();
-        list.createCycle(6, 4, head);
-        System.out.println(cycle.isCycle(head));
+        middleOfLinkedList middle = new middleOfLinkedList();
+        ListNode mid = middle.middle(head);
+        System.out.println(mid.data);
     }
 }
