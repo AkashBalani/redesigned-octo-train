2d5032442733e91306da529404a15e009d009ee4
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-04-21 17:31:57 +0530

    repeatedDNASequence #2

--- SlidingWindow/repeatedDNASequence.java
+++ SlidingWindow/repeatedDNASequence.java
@@ -1,5 +1,18 @@
 package SlidingWindow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class repeatedDNASequence {
-    
+    public List<String> findRepeatedSequences(String s, int k){
+        return new ArrayList<String>();
+    }
 }
+
+/*
+ * Given a string s that represents a DNA sequence, and a number k, return all the contiguous sequences 
+ * of length k that occur more than once in the string. The order of the returned subsequences does not 
+ * matter.
+ * If no repeated subsequence is found, the function should return an empty set.
+ * 
+ */
