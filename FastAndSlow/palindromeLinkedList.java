37824f46218ea7ff1d740a33bc30a3108e4cf9d4
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-04-21 16:08:15 +0530

    palindromeLinkedList #6

--- FastAndSlow/palindromeLinkedList.java
+++ FastAndSlow/palindromeLinkedList.java
@@ -5,6 +5,8 @@ import DataStructure.ListNode;
 
 public class palindromeLinkedList {
     public boolean isPalindrome(ListNode head) {
+        if(head == null)
+            return true;
         ListNode slow = head;                       
         ListNode fast = head;                  
         while(fast != null && fast.next != null){
@@ -27,4 +29,10 @@ public class palindromeLinkedList {
  * Constraints:
  * 1. The number of nodes in the list is in the range [1, 500]
  * 2. Node values from 0 to 9
+ * 
+ * Time Complexity:
+ * O(N) as it iterates over the Linked List once.
+ * 
+ * Space Complexity:
+ * O(1) as it uses constant space.
  */
