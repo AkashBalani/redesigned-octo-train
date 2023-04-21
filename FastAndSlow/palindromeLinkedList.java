e98b661f14e43bfc3f01195a8b832905468f775d
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-04-21 14:08:14 +0530

    palindromeLinkedList #1

--- FastAndSlow/palindromeLinkedList.java
+++ FastAndSlow/palindromeLinkedList.java
@@ -3,3 +3,9 @@ package FastAndSlow;
 public class palindromeLinkedList {
     
 }
+
+/*
+ * Constraints:
+ * 1. The number of nodes in the list is in the range [1, 500]
+ * 2. Node values from 0 to 9
+ */
