4398b6ba85a9c2ca7ee87ea8826c2677ab617083
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-04-22 16:31:27 +0530

    repeatedDNASequence #4

--- SlidingWindow/repeatedDNASequence.java
+++ SlidingWindow/repeatedDNASequence.java
@@ -2,35 +2,69 @@ package SlidingWindow;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class repeatedDNASequence {
-    public List<String> findRepeatedSequences(String s, int k){
+    public List<Integer> findRepeatedSequences(String s, int k) {
         int windowSize = k;
-        int windowStart = 0;
-        List<Character> window = new ArrayList<>();
-        List<String> result = new ArrayList<>();
-        Map<String, Integer> map = new HashMap<String, Integer>();
-        int hashCode = 0;
-        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
-            window.add(s.charAt(windowEnd));
-            if(window.size() == windowSize){
-                if(map.containsKey(String.valueOf(window))){
-                    result.add(String.valueOf(window));
+        if (s.length() < windowSize)
+            return new ArrayList<>();
+
+        int base = 4;
+        int hiPlaceValue = (int) Math.pow(base, windowSize);
+
+        Map<Character, Integer> map = new HashMap<>() {
+            {
+                put('A', 1);
+                put('C', 2);
+                put('G', 3);
+                put('T', 4);
+            }
+        };
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            int val = map.get(temp);
+            numbers.add(val);
+        }
+        int hashing = 0;
+        Set<Integer> seen = new HashSet<Integer>();
+        Set<String> output = new HashSet<String>();
+
+        for (int start = 0; start < s.length() - windowSize + 1; ++start) {
+            if (start != 0) {
+                hashing = hashing * base;
+                hashing -= numbers.get(start - 1) * hiPlaceValue;
+                hashing += numbers.get(start + windowSize - 1);
+                System.out.print("\tHash of current subsequence, [");
+                for (int i = start; i < start + windowSize; i++) {
+                    System.out.print(s.charAt(i));
                 }
-                map.put(String.valueOf(window), map.get(String.valueOf(window)) + 1);
-                hashCode = window.hashCode();
-                window.remove(windowStart++);
+                System.out.println("] = " + hashing);
+            } else {
+                for (int j = 0; j < windowSize; j++) {
+                    hashing = hashing * base + numbers.get(j);
+                    System.out.print("\tCalculating hash of first subsequence, ");
+                    for (int i = 0; i < j + 1; i++) {
+                        System.out.print(s.charAt(i));
+                    }
+                    System.out.println("= " + hashing);
+                }
             }
         }
-        return result;
+        return numbers;
     }
 }
 
 /*
- * Given a string s that represents a DNA sequence, and a number k, return all the contiguous sequences 
- * of length k that occur more than once in the string. The order of the returned subsequences does not 
+ * Given a string s that represents a DNA sequence, and a number k, return all
+ * the contiguous sequences
+ * of length k that occur more than once in the string. The order of the
+ * returned subsequences does not
  * matter.
  * If no repeated subsequence is found, the function should return an empty set.
  * 
--- main.java
+++ main.java
@@ -7,7 +7,7 @@ import SlidingWindow.repeatedDNASequence;
 public class main {
     public static void main(String[] args) {
         repeatedDNASequence sequence = new repeatedDNASequence();
-        List<String> result = sequence.findRepeatedSequences("AGCTGAAAGCTTAGCTG", 5);;
+        List<Integer> result = sequence.findRepeatedSequences("AGCTGAAAGCTTAGCTG", 5);;
         System.out.println(result);
     }
 }
