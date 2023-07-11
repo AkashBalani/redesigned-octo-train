a6df40c0329cfd4a77759b798dd507a5da9e6727
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-07-11 15:33:36 -0400

    Added code for Max in Sliding Window #6

---
+++ SlidingWindow/SlidingWindowMaximum.java
@@ -0,0 +1,11 @@
+package SlidingWindow;
+
+/**
+ * SlidingWindowMaximum
+ */
+public class SlidingWindowMaximum {
+
+    public static int[] findMaxSlidingWindow(int[] nums, int w){
+        return new int[0];
+    }
+}
