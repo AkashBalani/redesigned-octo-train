d9737e04a446bb5882722af2b4f40a48b9c52d4f
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2023-07-11 16:18:10 -0400

    Added code for Max in Sliding Window #8

--- SlidingWindow/SlidingWindowMaximum.java
+++ SlidingWindow/SlidingWindowMaximum.java
@@ -1,8 +1,11 @@
 package SlidingWindow;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
@@ -10,8 +13,31 @@ import java.util.Queue;
  */
 public class SlidingWindowMaximum {
 
-    public static int[] findMaxSlidingWindow(int[] nums, int w){
+    public int[] findMaxSlidingWindow(int[] nums, int w){
 
-        
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(w, Comparator.reverseOrder());
+        List<Integer> helperList = new ArrayList<>();
+        int max = 0;
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(queue.size() < w){
+                queue.add(nums[i]);
+                helperList.add(nums[i]);
+            }
+            else{
+                max = queue.peek();
+                list.add(max);
+                queue.remove(helperList.get(0));
+                helperList.remove(0);
+                queue.add(nums[i]);
+                helperList.add(nums[i]);
+            }
+            if(i == nums.length - 1){
+                max = queue.peek();
+                list.add(max);
+            }
+        }
+        int[] result = list.stream().mapToInt(i->i).toArray();
+        return result;
     }
 }
--- main.java
+++ main.java
@@ -1,13 +1,17 @@
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-import SlidingWindow.repeatedDNASequence;
+import SlidingWindow.SlidingWindowMaximum;
+// import SlidingWindow.repeatedDNASequence;
 
 public class main {
     public static void main(String[] args) {
-        repeatedDNASequence sequence = new repeatedDNASequence();
-        List<String> result = sequence.usingLinearMethod("AGCTGAAAGCTTAGCTG", 5);
-        System.out.println(result);
+        // repeatedDNASequence sequence = new repeatedDNASequence();
+        // List<String> result = sequence.usingLinearMethod("AGCTGAAAGCTTAGCTG", 5);
+        // System.out.println(result);
+
+        SlidingWindowMaximum program = new SlidingWindowMaximum();
+        int[] answer = program.findMaxSlidingWindow(new int[]{-4, 5, 4, -4, 4, 6, 7, 20}, 2);
+        System.out.println(Arrays.toString(answer));
     }
 }
